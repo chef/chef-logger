1d3e3cf220d29f8bc6d5ba2ae32ba611acf56423
dmaddu <dmaddu@progress.com>
2023-09-27 19:28:49 +0530

    fixing logger
    Signed-off-by: Durga Sarat Chandra Maddu <dmaddu@progress.com>

--- logger_test.go
+++ logger_test.go
@@ -1,78 +1,73 @@
 package chef_logger
 
 import (
+	"github.com/stretchr/testify/assert"
 	"os"
 	"testing"
 	// "regexp"
 )
 
-// BasicTestLogInit calls BasicLogger.LogInit() which should return nothing
-func BasicTestLogInit(t *testing.T) {
+
+func TestLogInit(t *testing.T) {
 	var log = Logger{}
 
-    c := new(Configuration)
-    c.LogToStdout = true
-    c.LogToFile= true
-    c.LogFileDirectory= "."
-    c.LogFilePrefix= "sample"
-    c.LogFileExtension= ".log"
-    // c.LogFileMaxSizeMB= 10
-    c.LogAsTelemetry= false
-    // c.LogTelemetryURI= ""
-    // c.LogTelemetryProtocol= ""
-    c.LogTimestampInUTC= true
-    c.LogLineFormat= StructuredText
-    c.LogServerName, _ = os.Hostname()
-    // c.LogServerIP= "1.1.1.1"
-    c.LogProcessName= "courier"
-    c.LogLevel= Information
-    // c.LogImplementationHint= Zap
+	c := new(Configuration)
+	c.LogToStdout = true
+	c.LogToFile = true
+	c.LogFileDirectory = "."
+	c.LogFilePrefix = "sample"
+	c.LogFileExtension = ".log"
+	// c.LogFileMaxSizeMB= 10
+	c.LogAsTelemetry = false
+	// c.LogTelemetryURI= ""
+	// c.LogTelemetryProtocol= ""
+	c.LogTimestampInUTC = true
+	c.LogLineFormat = StructuredText
+	c.LogServerName, _ = os.Hostname()
+	// c.LogServerIP= "1.1.1.1"
+	c.LogProcessName = "courier"
+	c.LogLevel = Information
+	// c.LogImplementationHint= Zap
 
-    // other way to define
-    /*
-    d := &Configuration{
-        LogServerName:  "",
-      } */
-    err := log.Configure(*c)
-        
-        // IP - // could https://www.golinuxcloud.com/golang-get-ip-address/
-    if err != nil {
-        t.Fatalf(`BasicLogger.LogInit("") = %v, want "", error`, err)
-    }
+	err := log.Configure(*c)
+	assert.NoError(t, err)
 }
 
-// BasicTestLogDebug calls BasicLogger.LogDebug() which should return nothing
-func BasicTestLogDebug (t *testing.T) {
-    var log = Logger{}
-    _, err := log.LogDebug("hello world")
-    if err != nil {
-         t.Fatalf(`BasicLogger.LogDebug("") = %v, want "", error`, err)
-    }
-}
-
-// BasicTestLogWarn calls BasicLogger.LogWarn() which should return nothing
-func BasicTestLogWarn (t *testing.T) {
-    var log = Logger{}
-    _, err := log.LogWarn(" *** warn **** ")
-    if err != nil {
-         t.Fatalf(`BasicLogger.LogWarn("") = %v, want "", error`, err)
-    }
-}
-
-// BasicTestLogError calls BasicLogger.LogError() which should return nothing
-func BasicTestLogError (t *testing.T) {
+func TestLogDebug(t *testing.T) {
 	var log = Logger{}
-    _, err := log.LogError(" bye-bye ")
-    if err != nil {
-         t.Fatalf(`BasicLogger.LogError("") = %v, want "", error`, err)
-    }
+	err := log.Configure(Configuration{
+		LogToStdout: true,
+		LogLevel:    Debug,
+	})
+	assert.NoError(t, err)
+	_, err = log.LogDebug("hello world")
+	assert.NoError(t, err)
 }
 
-// BasicTestLogClose calls BasicLogger.LogClose() which should return nothing
-func BasicTestLogClose (t *testing.T) {
-    var log = Logger{}
-    err := log.LogClose()
-    if err != nil {
-         t.Fatalf(`BasicLogger.LogClose("") = %v, want "", error`, err)
-    }
+func TestLogWarn(t *testing.T) {
+	var log = Logger{}
+	err := log.Configure(Configuration{
+		LogToStdout: true,
+		LogLevel:    Warning,
+	})
+	assert.NoError(t, err)
+	_, err = log.LogWarn(" *** warn **** ")
+	assert.NoError(t, err)
+}
+
+func TestLogError(t *testing.T) {
+	var log = Logger{}
+	err := log.Configure(Configuration{
+		LogToStdout: true,
+		LogLevel:    Error,
+	})
+	assert.NoError(t, err)
+	_, err = log.LogError(" bye-bye ")
+	assert.NoError(t, err)
+}
+
+func TestLogClose(t *testing.T) {
+	var log = Logger{}
+	err := log.LogClose()
+	assert.NoError(t, err)
 }
